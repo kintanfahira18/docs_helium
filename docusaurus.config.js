6a480622d15c5c9b3239c7c9127c07872dfed59c
Rahmat Ibrahim <rahmatibrahim141@gmail.com>
2023-10-19 15:52:59 +0700

    config docusaurus use env

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,85 +1,92 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+require("dotenv").config();
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const domainUrl = process.env.DOMAIN_URL;
+const baseUrl = process.env.BASE_URL;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Helium',
-  tagline: 'Simplify Security Testing',
-  favicon: 'img/favicon-helium.png',
+  title: "Helium",
+  tagline: "Simplify Security Testing",
+  favicon: "img/favicon-helium.png",
 
   // Set the production url of your site here
-  url: 'https://docs-helium.netlify.app',
+  url: `${domainUrl}`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: `${baseUrl}`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'cyberarmy', // Usually your GitHub org/user name.
-  projectName: 'docs_helium', // Usually your repo name.
+  organizationName: "cyberarmy", // Usually your GitHub org/user name.
+  projectName: "user_guide", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
+  clientModules: [require.resolve("./src/css/docs.css")],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/helium-social-card.jpg',
+      image: "img/helium-social-card.jpg",
       navbar: {
-        title: 'Helium',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "helium-logo",
+          src: "https://helium-sec-dev.s3.ap-southeast-1.amazonaws.com/general/helium-landing.png",
+          target: "_self",
+          href: domainUrl,
+          width: 200,
+          height: 80,
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'DOCS',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Home",
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'API Reference',
-            position: 'right',
+            href: `${domainUrl}/docs`,
+            label: "API Reference",
+            position: "right",
           },
         ],
       },
